fa33bf588eed65469613aecb25324443af031bcd
hicetnunc2000 <180111973@aluno.unb.br>
2020-07-05 04:09:11 +0000

    gobash

--- lotus-install.sh
+++ lotus-install.sh
@@ -10,13 +10,9 @@ sudo apt upgrade
 
 # https://golang.org/doc/install
 
-curl -O https://storage.googleapis.com/golang/go1.14.4.linux-amd64.tar.gz
-sudo mv go /usr/local
-export GOPATH=$HOME/work
-export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
-source ~/.profile
-go version
-
+./go-install.sh
+source $HOME/.bashrc
+go clean -modcache
 # Clone the Lotus repository and Build the Lotus binaries from source and install
 ﻿
 git clone https://github.com/filecoin-project/lotus.git
